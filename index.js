9e113b74af69e8fd2b88e781d053211fae09b591
pha-ran <pha-ran@naver.com>
2022-05-08 19:54:13 +0900

    Add multi-chat

--- index.js
+++ index.js
@@ -3,18 +3,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var port = 8080;
+var port = 15000;
+var users = 0;
 
 app.get('/', function(req, res) {
-  res.send('Word Chain');
+  res.render('index');
 });
 
-app.listen(port, function(){
-  console.log('server on! : ' + port);
+io.on('connection', (socket) => {
+  var name = socket.id;
+  users++;
+
+  io.to(socket.id).emit('change_name', name);
+  io.emit('response_message', '[ user connect : ' + socket.id + " (현재 채팅방 인원 :  " + users + ") ]");
+
+  socket.on('request_message', (name, text) => {
+    var msg = name + ' : ' + text;
+    io.emit('response_message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('response_message', '[ user disconnect : ' + socket.id + " ]");
+    users--;
+  });
+});
+
+http.listen(port, function(){
+  console.log('server on : ' + port);
 });
